d54953a9520efe588f62507a0c5cb97f0d2c4e78
fahrulKhoiruzaki <244107020234@student.polinema.ac.id>
2026-02-24 01:26:36 +0700

    Praktikum_Pertemuan2

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        return 'Selamat Datang';
+    }
+    public function about(){
+        return 'NIM : 244107020234  Nama : Fahrul Khoiruzaki';
+    }
+    public function articles($id){
+        return 'Halaman Artikel dengan ID: ' .$id;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::get('/greeting', [WelcomeController::class, 'greeting']);
