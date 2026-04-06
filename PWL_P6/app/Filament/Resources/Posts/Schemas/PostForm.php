ba9f6b553009d1daf00db594667e69b7b161e88e
fahrulKhoiruzaki <244107020234@student.polinema.ac.id>
2026-04-06 22:48:11 +0700

    P6_Jobsheet_02&03

--- PWL_P6/app/Filament/Resources/Posts/Schemas/PostForm.php
+++ PWL_P6/app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Posts\Schemas;
 
+use BladeUI\Icons\Components\Icon;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
@@ -12,6 +13,11 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Schemas\Components\Section;
+use Filament\Support\Icons\Heroicon;
+use Filament\Schemas\Components\Group;
+
+use function Laravel\Prompts\select;
 
 class PostForm
 {
@@ -19,23 +25,60 @@ class PostForm
     {
         return $schema
             ->components([
-                TextInput::make("title")
-                ->minLength(5),
-                TextInput::make("slug")
-                ->unique(),
-                Select::make("category_id")
-                    ->relationship("category", "name")
-                    ->preload()
-                    ->searchable(),
-                ColorPicker::make("color"),
-                // MarkdownEditor::make("content"),
-                RichEditor::make("content"),
-                FileUpload::make("image")
-                    ->disk("public")
-                    ->directory("posts"),
-                TagsInput::make("tags"),
-                Checkbox::make("published"),
-                DateTimePicker::make("published_at"),
-            ]);
+                // KIRI (2/3)
+                Group::make([
+                    Section::make("Post Details")
+                        ->description("Fill in the details of the post")
+                        ->icon('heroicon-o-document-text')
+                        ->schema([
+                            Group::make([
+                                TextInput::make('title')
+                                    ->required()
+                                    ->rules(['min:5'])
+                                    ->validationMessages([
+                                        'min' => 'Title minimal 5 karakter'
+                                    ]),
+                                TextInput::make('slug')
+                                    ->required()
+                                    ->unique()
+                                    ->rules(['min:3'])
+                                    ->validationMessages([
+                                        'min' => 'Slug minimal 3 karakter',
+                                        'unique' => 'Slug harus unik',
+                                    ]),
+                                Select::make('category_id')
+                                    ->relationship('category', 'name')
+                                    ->required()
+                                    ->searchable()
+                                    ->preload(),
+                                ColorPicker::make('color'),
+                            ])->columns(2),
+
+                            MarkdownEditor::make("content")
+                                ->columnSpanFull(),
+                        ]),
+                ])->columnSpan(2),
+
+                // KANAN (1/3)
+                Group::make([
+                    Section::make("Image Upload")
+                        ->icon('heroicon-o-photo')
+                        ->schema([
+                            FileUpload::make("image")
+                                ->required()
+                                ->disk("public")
+                                ->directory("posts"),
+                        ]),
+
+                    Section::make("Meta Information")
+                        ->icon('heroicon-o-information-circle')
+                        ->schema([
+                            TagsInput::make("tags"),
+                            Checkbox::make("published"),
+                            DateTimePicker::make("published_at"),
+                        ]),
+                ])->columnSpan(1),
+            ])
+            ->columns(3);
     }
 }
