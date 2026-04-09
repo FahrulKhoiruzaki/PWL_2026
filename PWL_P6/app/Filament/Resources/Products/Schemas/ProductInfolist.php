e7835d88cd05a042947679fa87061f574d462df6
fahrulKhoiruzaki <244107020234@student.polinema.ac.id>
2026-04-10 00:04:03 +0700

    P7_Jobsheet_3

--- PWL_P6/app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ PWL_P6/app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -6,6 +6,8 @@ use Filament\Schemas\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\IconEntry;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Schemas\Schema;
 
 use function Laravel\Prompts\number;
@@ -16,6 +18,68 @@ class ProductInfolist
     {
         return $schema
             ->components([
+                Tabs::make('Product Tabs')
+                ->tabs([
+                Tab::make('Product Details')
+                    ->icon('heroicon-o-academic-cap')
+                    ->schema([
+                        TextEntry::make('name')
+                            ->label('Product Name')
+                            ->weight('bold')
+                            ->color('primary'),
+                        TextEntry::make('id')
+                            ->label('Product ID'),
+                        TextEntry::make('sku')
+                            ->label('Product SKU')
+                            ->badge()
+                            ->color('success'),
+                        TextEntry::make('description')
+                            ->label('Product Description'),
+                        TextEntry::make('created_at')
+                            ->label('Product Creation Date')
+                            ->date('d M Y')
+                            ->color('info'),
+                    ]),
+                Tab::make('Product Price and Stock')
+                    ->icon('heroicon-o-currency-dollar')
+                    ->badge(fn ($record) => $record->stock)
+                    ->badgeColor('info')
+                    ->schema([
+                        TextEntry::make('price')
+                            ->label('Product Price')
+                            ->weight('bold')
+                            ->color('primary')
+                            ->icon('heroicon-o-currency-dollar'),
+                        TextEntry::make('stock')
+                            ->label('Product Stock'),
+                    ]),
+                Tab::make('Image and Status')
+                    ->icon('heroicon-o-photo')
+                    ->schema([
+                        ImageEntry::make('image')
+                            ->label('Product Image')
+                            ->disk('public'),
+                        TextEntry::make('price')
+                            ->label('Product Price')
+                            ->weight('bold')
+                            ->color('primary')
+                            ->icon('heroicon-o-currency-dollar'),
+                        TextEntry::make('stock')
+                            ->label('Product Stock')
+                            ->weight('bold')
+                            ->color('primary'),
+                        IconEntry::make('is_active')
+                            ->label('Is Active?')
+                            ->boolean(),
+                        IconEntry::make('is_featured')
+                            ->label('Is Featured')
+                            ->boolean(),
+                    ])
+                ])
+                ->columnSpanFull()
+                ->vertical(),
+                
+
                 Section::make('Product Info')
                     ->description('')
                     ->schema([
@@ -37,7 +101,6 @@ class ProductInfolist
                             ->color('info'),
                     ])
                 ->columnSpanFull(),
-                
                 Section::make('Product Price and Stock')
                     ->description('')
                     ->schema([
@@ -52,7 +115,6 @@ class ProductInfolist
                             ->icon('heroicon-o-cube'),
                     ])
                 ->columnSpanFull(),
-
                 Section::make('Image and Status')
                     ->description('')
                     ->schema([
