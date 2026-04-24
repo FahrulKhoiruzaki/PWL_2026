5007b5bb2188c56e39d8922cde409e4432d7bb02
fahrulKhoiruzaki <244107020234@student.polinema.ac.id>
2026-04-25 01:41:08 +0700

    P9_Jobsheet_1&2

---
+++ PWL_P9/app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Schemas;
+
+use BladeUI\Icons\Components\Icon;
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Schemas\Components\Section;
+use Filament\Support\Icons\Heroicon;
+use Filament\Schemas\Components\Group;
+
+use function Laravel\Prompts\select;
+
+class PostForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                // KIRI (2/3)
+                Group::make([
+                    Section::make("Post Details")
+                        ->description("Fill in the details of the post")
+                        ->icon('heroicon-o-document-text')
+                        ->schema([
+                            Group::make([
+                                TextInput::make('title')
+                                    ->required()
+                                    ->rules(['min:5'])
+                                    ->validationMessages([
+                                        'min' => 'Title minimal 5 karakter'
+                                    ]),
+                                TextInput::make('slug')
+                                    ->required()
+                                    ->unique()
+                                    ->rules(['min:3'])
+                                    ->validationMessages([
+                                        'min' => 'Slug minimal 3 karakter',
+                                        'unique' => 'Slug harus unik',
+                                    ]),
+                                Select::make('category_id')
+                                    ->relationship('category', 'name')
+                                    ->required()
+                                    ->searchable()
+                                    ->preload(),
+                                ColorPicker::make('color'),
+                            ])->columns(2),
+
+                            MarkdownEditor::make("content")
+                                ->columnSpanFull(),
+                        ]),
+                ])->columnSpan(2),
+
+                // KANAN (1/3)
+                Group::make([
+                    Section::make("Image Upload")
+                        ->icon('heroicon-o-photo')
+                        ->schema([
+                            FileUpload::make("image")
+                                ->required()
+                                ->disk("public")
+                                ->directory("posts"),
+                        ]),
+
+                    Section::make("Meta Information")
+                        ->icon('heroicon-o-information-circle')
+                        ->schema([
+                            TagsInput::make("tags"),
+                            Checkbox::make("published"),
+                            DateTimePicker::make("published_at"),
+                        ]),
+                ])->columnSpan(1),
+            ])
+            ->columns(3);
+    }
+}
